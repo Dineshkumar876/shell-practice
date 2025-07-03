4eab64e45e33caeb00fe141e514811a174e2acaa
Dinesh <dinesh@vasa.com>
2025-07-03 16:32:07 +0530

    conditions is to

--- variables.sh
+++ variables.sh
@@ -3,10 +3,12 @@
  echo "All variables passed: $@"
  echo "Number of variables passed: $#"
  echo "script name: $0"
- echo "current working directory: $pwd"
- echo "home is running the script: $home"
- echo "user is running the script: $user"
- echo "pid of the current script: $$"
- echo "sleep 10 sec"
+ echo "current working directory: $PWD"
+ echo "Home is running the script: $home"
+ echo "User is running the script: $user"
+ echo "PID of the current script: $$"
+ sleep 10 &
  echo "command running in background: $!"
-   
+
+ echo "ps -ef |grep sleep"
+
