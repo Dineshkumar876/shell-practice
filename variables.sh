a8fb6b1110f84d38c267720998f0f7d2ffee42b6
Dinesh <dinesh@vasa.com>
2025-07-03 15:44:09 +0530

    variables is to

---
+++ variables.sh
@@ -0,0 +1,12 @@
+ #!/bin/bash
+
+ echo "All variables passed: $@"
+ echo "Number of variables passed: $#"
+ echo "script name: $0"
+ echo "current working directory: $pwd"
+ echo "home is running the script: $home"
+ echo "user is running the script: $user"
+ echo "pid of the current script: $$"
+ echo "sleep 10 sec"
+ echo "command running in background: $!"
+   
